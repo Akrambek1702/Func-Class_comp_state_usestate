151b4d928e9b04d0f267ed88a73377f9fc4b36c8
Akrambek1702 <akram4ik1717@gmail.com>
2022-08-11 15:56:30 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [spent, setSpent] = useState([
+    {description: 'food', amount: 400},
+    {description: 'fruit', amount: 300}
+  ]);
+
+const handleClick = () => {
+  setSpent([...spent, {
+     description: document.getElementById("desc").value,
+     amount: document.getElementById("amount").value
+  }])
+  document.getElementById("desc").value = "";
+  document.getElementById("amount").value = "";
+  console.log(spent);
+}; 
+
+
+  return (
+    <div className="App">
+       <input type="text" id="desc" />
+       <input type="text" id="amount" />
+       <button onClick={handleClick}>Add</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ClassComp.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ClassComp extends Component {
+    constructor() {
+        super();
+    }
+    
+  render() {
+    return (
+      <div>ClassComp</div>
+    )
+  }
+}
+
+export default ClassComp;
